1c56bf3b9573e772c865650bbe61cd058c001571
Vasiliy Rusin <RusinVA@gmail.com>
2021-05-28 18:37:55 +0700

    Update export

--- index.ts
+++ index.ts
@@ -1,12 +1,1 @@
-import "./docs/main";
-// import { ImageManager } from "./src";
-
-// const imageManager = new ImageManager();
-// const image = imageManager.createImage({
-//   width: 1012,
-//   height: 300,
-//   fontWieght: 700,
-//   color: "#000000",
-//   backgroundColor: "#f7e017",
-//   text: "🎉  DummyImageJS  🎉"
-// });
+export { Image, ImageManager, Manager } from "./src";
